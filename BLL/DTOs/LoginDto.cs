2ba0f26a55eff41a96e1a9e4cc43a86db592fb7e
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-05-09 20:28:43 +0600

    feat: add DTOs for user, doctor, appointment, and authentication

---
+++ BLL/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DTOs;
+
+public class LoginDto
+{
+    [Required] public string Email { get; set; }
+
+    [Required] public string Password { get; set; }
+}
