96b2ed0b485f213173b5975c9153c6e1a59d9eb1
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-05-09 20:22:17 +0600

    feat: register repositories and services in builder

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,13 +1,34 @@
+using BLL.Services;
 using DAL.Context;
+using DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"))
 );
+
+// DAL
+builder.Services.AddScoped(typeof(GenericRepository<>));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<DoctorRepository>();
+builder.Services.AddScoped<PatientRepository>();
+builder.Services.AddScoped<AppointmentRepository>();
+builder.Services.AddScoped<DepartmentRepository>();
+
+// BLL
+builder.Services.AddScoped(typeof(GenericService<>));
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<DoctorService>();
+builder.Services.AddScoped<PatientService>();
+builder.Services.AddScoped<AppointmentService>();
+builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<PrescriptionService>();
 
 var app = builder.Build();
 
